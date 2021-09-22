6155243f3f42d3a9be7dba81cdacb22f8627669d
Madhavi Latha Yayavaram <87151264+MadhaviLathaYayavaram@users.noreply.github.com>
2021-09-22 23:39:10 +0530

    Add files via upload

---
+++ TitleCaseArray.js
@@ -0,0 +1,11 @@
+// Convert all the strings to title caps in a string array
+
+var myString = "guvi zen student"
+function titleCaseString(s) {
+  st = s.toLowerCase().split(' ');
+  for (var i = 0; i < st.length; i++) {
+    st[i] = st[i].charAt(0).toUpperCase() + st[i].slice(1); 
+  }
+  return st.join(' ');
+}
+console.log (titleCaseString(myString));
